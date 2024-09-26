64a6ff0cb22dc2ba5fb883a8e1ae1e07cc311263
i-am-ivan <jovicorp.studio@gmail.com>
2024-09-26 17:37:59 +0300

    Deploy Version 1.0.1 to repo

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,23 @@ Route::get('/', function () {
     return view('adminLogin', ['name' => 'i-am-ivan']);
 });
 
-Route::get('/adminDashboard', function(){
+Route::get('/Admin/adminDashboard', function(){
     return view('adminDashboard', ['heading' => 'Admin Dashboard']);
 });
+
+Route::get('/Admin/userProfile', function(){
+    return view('userProfile', ['heading' => 'My Profile']);
+});
+
+Route::get('/Admin/loanManagementDashboard', function(){
+    return view('loanManagementDashboard', ['heading' => 'Loans']);
+});
+
+Route::get('/Admin/customerManagementDashboard', function(){
+    return view('customerManagementDashboard', ['heading' => 'Customers']);
+});
+
+Route::get('/Admin/usersManagementDashboard', function(){
+    return view('usersManagementDashboard', ['heading' => 'Users']);
+});
+
